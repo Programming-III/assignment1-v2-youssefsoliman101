66691102ef2671def3860f4a070b6c1c98c47938
youssefsoliman101 <youssef.soliman@student.giu-uni.de>
2025-11-11 09:43:07 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,36 @@
-#ifndef INSTRUCTOR_H
-#define INSTRUCTOR_H
+//
+// Created by Youssef Soliman on 11/11/2025.
+//
+#include <iostream>
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+#ifndef UNTITLED9_INSTRUCTOR_H
+#define UNTITLED9_INSTRUCTOR_H
 
 
+class Instructor:public Person{
+private:
+    string department;
+    int experienceYears;
+public:
+ Instructor() {
+
+}
+Instructor(string d, int ey) {
+    department=d;
+    experienceYears=ey;
+}
+display() {
+    cout<<"Department: "<<department<<endl;
+    cout<<"Experience Years: "<<experienceYears<<endl;
+}
+~Instructor() {
+
+}
 
 
+};
 
 
-
-
-
-
-
-
-#endif
+#endif //UNTITLED9_INSTRUCTOR_H

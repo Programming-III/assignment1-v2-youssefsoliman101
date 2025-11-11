009ea2204125d72ad4bc25200678512a00bd3ac2
youssefsoliman101 <youssef.soliman@student.giu-uni.de>
2025-11-11 09:40:21 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,31 @@
-#ifndef COURSE_H
-#define COURSE_H
-
-#include <string>
+//
+// Created by Youssef Soliman on 11/11/2025.
+//
+#include <iostream>
 #include "Student.h"
 using namespace std;
-//#write your code here
+#ifndef UNTITLED9_COURSE_H
+#define UNTITLED9_COURSE_H
 
 
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+
+public:
+
+    Course();
+    Course(string courseCode,string courseName,int maxStudents,Student* students,int currentStudents);
+    void addStudent(const Student& s);
+    void displayCourseInfo();
+    ~Course();
+    string getcourseCode();
+    string getcourseName();
+};
 
 
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif //UNTITLED9_COURSE_H

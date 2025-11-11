a7c560cd954f61b7cb25da7b554a501a81e71e1a
youssefsoliman101 <youssef.soliman@student.giu-uni.de>
2025-11-11 09:45:44 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,21 +1,35 @@
-#ifndef STUDENT_H
-#define STUDENT_H
+//
+// Created by Youssef Soliman on 11/11/2025.
+//
+#include <iostream>
 
 #include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
+using namespace std;
+#ifndef UNTITLED9_STUDENT_H
+#define UNTITLED9_STUDENT_H
 
 
+class Student:public Person{
+private:
+    int yearLevel;
+    string major;
+public:
+  Student() {
+
+}
+
+void display() {
+    cout<<"Year Level: "<<yearLevel<<endl;
+    cout<<"Major: "<<major<<endl;
+}
+Student(int yl, string m) {
+    yearLevel=yl;
+    major=m;
+}
+~Student() {
+
+}
+};
 
 
-
-
-
-
-
-
-
-
-
-#endif
+#endif //UNTITLED9_STUDENT_H
